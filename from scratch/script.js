f3f5852d9501a318c7d7b1f0b188fe857dff3b04
RezaAstaraki <reza.astaraky@gmail.com>
2023-11-06 22:06:56 +0330

    chnage meu need to fix

---
+++ from scratch/script.js
@@ -0,0 +1,7 @@
+
+var btn_clollapse = document.getElementById('colapse_button')
+btn_clollapse.addEventListener('click',()=>{
+    const menu = document.getElementById('menu')
+    menu.classList.toggle('vertical_menu')
+
+})
