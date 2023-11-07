01e89c8d13b479d29fec0fd1e1305d1b483316ad
Omidi-a <omidiazam325@gmail.com>
2023-11-07 21:56:32 +0330

    finished menu add about me

--- from scratch/script.js
+++ from scratch/script.js
@@ -1,7 +1,17 @@
+var btn_collapse = document.getElementById('colapse_button');
+btn_collapse.addEventListener('click', () => {
+    var menu = document.getElementById('menu');
+    var menu_ul = document.getElementById('menu_ul');
+    menu_ul.classList.toggle('vertical_menu');
+    menu.classList.toggle('vertical_menu_top');
+});
 
-var btn_clollapse = document.getElementById('colapse_button')
-btn_clollapse.addEventListener('click',()=>{
-    const menu = document.getElementById('menu')
-    menu.classList.toggle('vertical_menu')
-
-})
+window.addEventListener('resize', () => {
+    const innerWidth = window.innerWidth;
+    if (innerWidth > 775) {
+        var menu = document.getElementById('menu');
+        var menu_ul = document.getElementById('menu_ul');
+        menu_ul.classList.remove('vertical_menu'); // Remove the class 'vertical_menu'
+        menu.classList.remove('vertical_menu'); // Remove the class 'vertical_menu'
+    }
+});
